a262e07364b2fda3b350c340538e00b2cedbe77e
Carlosmicael <carlosmlo381@gmail.com>
2025-07-01 12:42:16 -0500

    definitivo

---
+++ apps/django_Comersio_bf/apps/camara_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CamaraAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'camara_app'
---
+++ apps/django_Comersio_bf/config/settings/base.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+import sys
+
+BASE_DIR = Path(__file__).resolve().parent.parent 
+sys.path.insert(0, str(BASE_DIR))
